374b3393d63dadd94252eaa9eb99541657080c77
yhlmaybe <496041257@qq.com>
2024-03-16 01:55:56 -0700

    update

--- ROSManager/NodeManager.cpp
+++ ROSManager/NodeManager.cpp
@@ -1,6 +1,7 @@
 #include "NodeManager.h"
 
-ServoDriveNodeListener::ServoDriveNodeListener(std::map<int, Servo> servoKeyValues) : Node("serve_drive_node_listener"), servoKeyValues(std::move(servoKeyValues))
+ServoDriveNodeListener::ServoDriveNodeListener(std::map<int, Servo> servoKeyValues) 
+    : Node("serve_drive_node_listener"), servoKeyValues(std::move(servoKeyValues))
 {
     subscription = this->create_subscription<std_msgs::msg::String>(
        "ServeMsg", 10, std::bind(&ServoDriveNodeListener::DoListen, this, std::placeholders::_1));
